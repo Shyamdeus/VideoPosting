37ab686159424730f4b67999fd60df1ba6db2440
Shyamdeus <shyamjerny1@gmail.com>
2025-07-21 19:35:02 +0530

    imagefix2

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ export default function App() {
       <header style={styles.header}>
         {/* Banner Image */}
         <img
-          src="src/images/maxresdefault.jpg"
+          src="public/images/maxresdefault.jpg"
           alt="Banner"
           style={styles.banner}
         />
@@ -14,7 +14,7 @@ export default function App() {
         {/* Logo + Title */}
         <div style={styles.logoContainer}>
           <img
-            src="src/images/bluebird.png"
+            src="public/images/bluebird.png"
             alt="Logo"
             style={styles.logo}
           />
